b8828bf296060a650e948b98220e4b44905100e7
Pezhman Zarabadi-Poor <pezhman.zarabadi-poor@wae.com>
2023-03-29 15:39:39 +0100

    migration

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ if __name__ == "__main__":
         name="jaffle",
         packages=find_packages(exclude=["jaffle_tests"]),
         install_requires=[
-            "dagster",
-            "pandas",
-            "duckdb",
-            "sqlescapy",
-            "html5lib",
-            "lxml",
+            "dagster==1.1.10",
+            "pandas==1.5.3",
+            "duckdb==0.6.1",
+            "sqlescapy==1.0.1",
+            "html5lib==1.1",
+            "lxml==4.9.2",
         ],
         extras_require={
-            "dev": ["dagit", "pytest", "localstack", "awscli", "awscli-local"]
+            "dev": ["dagit==1.1.10", "pytest", "localstack==1.0.4", "awscli==1.27.52", "awscli-local==0.20"]
         },
     )
